ce4add56c9e96e304cf6be0ccd3f5aedd55f6d26
EC2 Default User <ec2-user@ip-172-31-37-154.ap-northeast-1.compute.internal>
2023-10-15 03:52:45 +0000

    [Start] booksのテーブル表示直した

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about',as: 'about'
+  get 'home/about' => 'homes#about',as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
