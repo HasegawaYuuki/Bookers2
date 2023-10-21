3b96f21e72c0b6150f5eb21a704c72c4350714f1
EC2 Default User <ec2-user@ip-172-31-37-154.ap-northeast-1.compute.internal>
2023-10-21 07:47:58 +0000

    [Finish] コメント機能実装完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,25 +1,22 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_params)
-    comment.book_id = book.id
+    @book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: books_path)
+    redirect_back(fallback_location: books_path(@book))
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.comments.find_by(book_id: book.id)
-    comment.book_id = book.id
-    comment.destroy
+    BookComment.find(params[:id]).destroy
     redirect_back(fallback_location: books_path)
   end
 
 
   private
 
-  def book_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 end
