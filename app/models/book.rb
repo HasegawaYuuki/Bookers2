29209abe1e96b9b2d895e94a499ba54ffd2de399
EC2 Default User <ec2-user@ip-172-31-37-154.ap-northeast-1.compute.internal>
2023-11-05 07:46:40 +0000

    [Add] 新しい・古い・評価高い順に並び替え

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,10 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   
   has_many :read_counts, dependent: :destroy
+  
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
 
   validates :title, presence: true
   validates :body, presence: true,
